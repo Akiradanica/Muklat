52c3ccdd41cde8fdd844f87c6397b23f5ac338f9
Danica Longcop <100036627+Akiradanica@users.noreply.github.com>
2024-04-01 15:12:48 +0800

    Update Muklat_arduino_04-01-24.ino
    speaker and internet connection are not yet included in the code

--- Hardware/Muklat_arduino_04-01-24/Muklat_arduino_04-01-24.ino
+++ Hardware/Muklat_arduino_04-01-24/Muklat_arduino_04-01-24.ino
@@ -1,17 +1,25 @@
+//all are working well and just need to connect to the internet
+//buzzer, servo, lidar, vibrator motor
+//speaker not yet connected
+//as well not connected to the internet yet
+
+
 #include <Wire.h>
 #include <TFLI2C.h>
+#include <Servo.h>
 
+Servo servo;
 TFLI2C tflI2C;
 
 int16_t tfDist; // distance in centimeters
 int16_t tfAddr = TFL_DEF_ADR; // Use this default I2C address
 
-//int servoMotor = ;
+
 // int speakerPin = 6;
 
 //left 
 const int buzzer1Pin = 4;
-const int vibmotor1Pin = 12;
+const int vibmotor1Pin = 3;
 
 //right
 //int buzzer2Pin = 7;
@@ -21,6 +29,7 @@ void setup()
 {
   Serial.begin(115200); // Initalize serial port
   Wire.begin();         // Initalize Wire library
+  servo.attach(10);     //setting to PWM pin
   pinMode(buzzer1Pin, OUTPUT);
   //pinMode(buzzer2Pin, OUTPUT);
   pinMode(vibmotor1Pin, OUTPUT);
@@ -34,7 +43,7 @@ void loop()
   if (tflI2C.getData(tfDist, tfAddr)) 
   {
     Serial.println(String(tfDist) + " cm / " + String(tfDist / 100) + " m");
-    digitalWrite(vibmotor1Pin, HIGH);
+    
     // Check if the distance is less than a threshold (adjust as needed)
     if (tfDist < 50) 
     {
@@ -42,12 +51,17 @@ void loop()
       tone(buzzer1Pin, 400);
       // Activate vibration motor
       digitalWrite(vibmotor1Pin, HIGH);
+      servo.write(0);
+      delay(1000);
+      servo.write(180);
+      delay(1000);
     } 
     else
     {
       // Stop the buzzer and turn off vibration motor if the distance is greater than the threshold
       noTone(buzzer1Pin);
       digitalWrite(vibmotor1Pin, LOW);
+
     }
   }
   delay(50);
