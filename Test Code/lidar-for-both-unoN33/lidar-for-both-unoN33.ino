7febd340b1c2663c1a7211d920df6dbc2ef69e44
Danica Longcop <longcopdanica23@gmail.com>
2024-04-08 13:28:34 +0800

    Update lidar-for-both-unoN33.ino

--- Test Code/lidar-for-both-unoN33/lidar-for-both-unoN33.ino
+++ Test Code/lidar-for-both-unoN33/lidar-for-both-unoN33.ino
@@ -10,16 +10,20 @@ TFLI2C tflI2C;
 int16_t tfDist; // distance in centimeters
 int16_t tfAddr = TFL_DEF_ADR; // Use this default I2C address
 
+String lid_data;
 void setup() 
 {
   Serial.begin(115200); // Initalize serial port
   Wire.begin();         // Initalize Wire library
 }
 
-void loop() {
+void loop() 
+{
+  
   if (tflI2C.getData(tfDist, tfAddr)) 
   {
-    Serial.println(String(tfDist) + " cm / " + String(tfDist / 100) + " m");
+    String lid_data = String(tfDist) + " cm" + String(tfDist / 100) + " m";
+    Serial.println(lid_data);
   }
   delay(50);
 }
